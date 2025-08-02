6b5d48fec88ce5c1f90a75364e0704612452a5fa
Arjun-G-Ravi <arjungravi1@gmail.com>
2025-08-02 14:18:33 +0700

    Patch

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,79 @@
+if status is-interactive
+    starship init fish | source
+    zoxide init fish | source 
+	set -g fish_greeting ""
+    # Commands to run in interactive sessions can go here
+    bind \t forward-char
+    bind \cv fish_clipboard_paste
+    bind \cH backward-kill-word
+
+    # important for fzf; dont change
+    # set -g FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
+
+    # Setting path
+    set -Ux fish_user_paths $fish_user_paths /home/arjun/.local/bin
+
+    # My custom things
+    # alias ai 'code /home/arjun/Desktop/GitHub/AI-Notes && q'
+    # alias non-ai 'code /home/arjun/Desktop/GitHub/Non-AI-Notes && q'
+    alias ls 'ls -al'
+    alias bye 'echo "Goodbye" && shutdown now'
+    alias q 'exit'
+    # alias r 'ranger'
+    alias rm 'trash'
+    alias cd 'z'
+    alias cdi 'zi'
+    # alias g 'cd ~/Desktop/GitHub && r'
+    # alias noob 'cd ~/Desktop/GitHub/Noob-projects/ && code .'
+    # alias fuzzyfind 'code (find . -type d | fzf --preview="ls {}") && q'
+    alias c 'code .'
+    alias activate_AI_ENV '. ~/Desktop/AI_ENV/bin/activate.fish'
+    alias activate_ai_env '. ~/Desktop/AI_ENV/bin/activate.fish'
+    alias activate_kaggle_env '. ~/Desktop/kaggle_env/bin/activate.fish'
+    alias logout 'sudo pkill -u arjun' # my username
+
+    function cow
+        set inp $argv
+        if test -z "$argv"
+            git add .; and git commit -m "Patch"; and git push
+        else
+            git add .; and git commit -m "$argv"; and git push
+        end
+    end
+
+    function push_obsidian 
+        cd /home/arjun/Documents/general/_obsidian-backup;git add .; and git commit -m "Added more to obsidian"; and git push
+    end
+
+    function pull_obsidian
+    cd /home/arjun/Documents/general/_obsidian-backup; git pull
+    end
+
+    function nuke
+        if test -z $argv
+            echo "Usage: newkill <pattern>"
+            return 1
+        end
+        set pattern $argv[1]
+        set -l processes (pgrep -l $pattern)
+
+        if test -z "$processes"
+            echo "No processes found matching the pattern: $pattern"
+            return 1
+        else
+            echo "Processes matching the pattern '$pattern':"
+            for line in $processes
+                echo $line
+            end
+        end
+        echo "Do you want to kill these processes? (y/n)"
+        set confirm (read)
+        if test "$confirm" = "y"
+            sudo pkill -9 $pattern
+            echo "Processes killed."
+        else
+            echo "Action aborted."
+        end
+    end
+
+end
